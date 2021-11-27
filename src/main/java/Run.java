31faf38b192937926b7c8f71032968dcf51a9758
Jenny Tang <tangjenny1224@gmail.com>
2021-11-26 17:06:05 -0800

    fix run script

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,30 +1,31 @@
+import org.apache.log4j.PropertyConfigurator;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Run {
-
     public static void main(String[] args) throws IOException {
+        String log4jConfPath = "src/main/resource/log4j.properties";
+        PropertyConfigurator.configure(log4jConfPath);
         IdMap idMapRW = new IdMap();
         InvertedIndex invertedIndexRW = new InvertedIndex();
         List<String> files = new ArrayList<>();
 
         long startTime = System.currentTimeMillis();
+
         SentimentAnalysis.init();
         SentimentAnalysis.loadStopWords();
 
 
         files.add("data/yelp_academic_dataset_business_food.json");
-        List<Object> bizList = ProcessData.readFile(files, "business");
-        System.out.println("finishing reading business info file");
-        BusinessIdMap idMapBiz = (BusinessIdMap) bizList.get(0);
-
-
         files.add("data/yelp_academic_dataset_review_food.json");
+
+        List<Object> bizList = ProcessData.readFile(files, "business");
+        BusinessIdMap idMapBiz = (BusinessIdMap) bizList.get(0);
+        System.out.println("...Finishing reading business info file...");
+
         List<Object> rwList = ProcessData.readFile(files, "reviews");
-        System.out.println("finishing reading review file");
+        System.out.println("...Finishing reading review file...");
 
 
         for(Object o : rwList){
@@ -37,11 +38,10 @@ public class Run {
         }
 
         Search search = new Search();
-        String term = "donuts";
+        String term = "crepe";
         int k = 3;
-        Set<Integer> reviewIds = search.getReviewIds(term, invertedIndexRW);
-        Map<String, Integer> businessWithPosCnt = search.MakeBusinessWithPosRWsMap(idMapRW, reviewIds, term);
-        search.displayTopKBusiness(businessWithPosCnt, idMapBiz, k);
+        Map<String, Integer> businessWithPosCnt = search.MakeBusinessWithPosRWsMap(idMapRW, invertedIndexRW, term);
+        search.displayTopKBusiness(term, businessWithPosCnt, idMapBiz, k);
 
         long endTime = System.currentTimeMillis();
         long timeElapsed = endTime - startTime;
